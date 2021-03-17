2f35427ebbf1f6bb6bce7a2d4841d3357eead4ca
shelling94 <shelling4869.holmes@gmail.com>
2021-03-17 15:46:58 +0700

    1st upload to GitHub.

---
+++ src/math/util/MathUtil.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.util;
+
+/**
+ *
+ * @author NHULT
+ */
+public class MathUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+        System.out.println("This is the 1st upload that connects to remote server: GitHub");
+    }
+    
+}
